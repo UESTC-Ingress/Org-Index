d898688fa098ece0f3193bf5e7381df8a55a95bf
Charles Yang <Charles@0x7f.cc>
2020-03-05 22:21:17 +0800

    Not beta any more.

--- src/components/Home/Topic.vue
+++ src/components/Home/Topic.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    <b-btn block squared size="lg" variant="outline-danger">Test</b-btn>
+    <b-btn block squared size="lg" variant="outline-danger" disabled>暂无内容</b-btn>
   </div>
 </template>
